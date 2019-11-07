89e995536fa4dcd99552be29c1cf5ff861ee1117
Karin Skans <skka19st@student.ju.se>
2019-11-07 15:41:56 +0100

    ovning 1 summering av tva tal_version01

---
+++ summering/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace summering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            // skriver till skärmen
+            Console.WriteLine("Skriv ditt första tal");
+
+            // indata från skärmen är alltid i text-format
+            // indata måste konverteras innan beräkningar kan göras
+            var text1 = Console.ReadLine();
+            int tal1 = Convert.ToInt32(text1);
+
+            // skriver till skärmen
+            Console.WriteLine("Skriv ditt andra tal");
+
+            // indata från skärmen är alltid i text-format
+            // indata måste konverteras innan beräkningar kan göras
+            var text2 = Console.ReadLine();
+            int tal2 = Convert.ToInt32(text2);
+
+            // summera de båda talen
+            int summa = tal1 + tal2;
+
+            // skriver välkomsthälsning
+            Console.WriteLine("du angav " + tal1 + " och " + tal2);
+            Console.WriteLine("tillsammans blir det " + summa);
+        }
+    }
+}
