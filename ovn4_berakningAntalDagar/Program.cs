faf315fec7ecda7151462222a3095b73772c2877
Karin Skans <skka19st@student.ju.se>
2019-11-07 18:43:54 +0100

    ovning4 Inmatning och konvertering_version03

---
+++ ovn4_berakningAntalDagar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ovn4_berakningAntalDagar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hämtar in namn och skriver hälsning
+            Console.WriteLine("Hej, vad heter du?");
+            var namn = Console.ReadLine();
+
+            // '$' och '{}' är specialtecken för att kunna lägga 
+            // in variabler mitt i texten
+            Console.WriteLine($"Välkommen hit, {namn}!");
+            
+            // hämta ålder och beräkna antal dagar
+            Console.WriteLine("Hur gammal är du?");
+            string ageYear = Console.ReadLine();
+            int ageDays = Convert.ToInt32(ageYear) * 365;
+
+            Console.Write($"{ageYear} år! ");
+            Console.WriteLine($"Då har du levt i {ageDays} dagar.");
+        }
+    }
+}
